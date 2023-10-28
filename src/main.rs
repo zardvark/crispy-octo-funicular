705bb3acefe5ed35f1b190c5767fa976dd3dcb38
Justin <jgold885@gmail.com>
2023-10-27 21:49:53 -0400

    testing the kernel

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
 
 
 #[panic_handler]
@@ -26,15 +27,32 @@ pub extern "C" fn _start() -> ! {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion...");
-    assert_eq!(1,1);
-    println!("[ok]");
+    serial_print!("trivial assertion...");
+    assert_eq!(0, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
